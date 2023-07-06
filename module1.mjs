999e8b2e7777a500354eb647139c849d92c55b3d
utkarsh-009 <utkarshpatil321@gmail.com>
2023-07-07 00:42:17 +0530

    Updates

--- module1.mjs
+++
@@ -1,5 +0,0 @@
-import greet, {b,c,d} from "./module2.mjs"
-console.log(greet);
-console.log(b)
-console.log(c)
-console.log(d)
--- module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = "Hello";
-const b = "My";
-const c = "Name";
-const d = "Is";
-
-export default a;
-export {b}
-export {c}
-export {d}
